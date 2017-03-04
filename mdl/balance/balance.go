77ce0d74d3a767711c7249c9d2b42515ebefede8
Carlos Peña <gesaodin@gmail.com>
2017-03-04 00:06:21 -0400

    Control Archivos Esquemas

--- mdl/balance/balance.go
+++ mdl/balance/balance.go
@@ -183,7 +183,67 @@ func (p *Pago) GenerarCobrosYPagos(data Pago) (jSon []byte, err error) {
 func generarCobrosYPagosGeneral(fecha string) (s string) {
 
 	if fecha != "" {
-		fecha = ` lotepar.fech BETWEEN '` + fecha + ` 00:00:00'::TIMESTAMP AND '` + fecha + ` 23:59:59'::TIMESTAMP `
+		fecha = `SELECT saldo_agencia.obse, saldo_agencia.saldo,
+				debe.monto AS entregado, haber.monto AS recibido,
+				ingreso.monto AS ingreso, egreso.monto AS egreso,
+				prestamo.monto AS prestamo,
+				cobrosypagos.vien AS vienen,
+				cobrosypagos.van AS van
+			FROM (
+			SELECT agencia.obse, lotepar.fech, SUM(lotepar.saldo) AS saldo
+			FROM agencia
+			JOIN zr_agencia ON agencia.oid=zr_agencia.oida
+			JOIN (
+				SELECT arch, agen, fech, vent-prem-comi as saldo, vent, prem, comi, sist from loteria
+				UNION
+				SELECT arch, agen, fech, vent-prem-comi as saldo, vent, prem, comi, sist from parley
+			) AS lotepar ON zr_agencia.codi=lotepar.agen
+
+			WHERE ` + fecha + `
+			GROUP BY agencia.obse,lotepar.fech
+			) saldo_agencia
+
+			-- DEBE
+			LEFT JOIN (
+					SELECT agen, fapr, SUM(mont) AS monto FROM debe
+					GROUP BY agen,fapr
+			) AS debe ON
+			debe.agen=saldo_agencia.obse AND debe.fapr=saldo_agencia.fech
+
+			-- HABER
+			LEFT JOIN (
+				SELECT agen, fapr, SUM(mont) AS monto FROM haber
+				GROUP BY agen,fapr
+			) AS haber ON
+			haber.agen=saldo_agencia.obse  AND haber.fapr=saldo_agencia.fech
+
+			--INGRESO
+			LEFT JOIN (
+				SELECT agen, fech, SUM(mont) AS monto FROM movimiento_ingreso
+				GROUP BY agen,fech
+			)
+			AS ingreso ON
+			ingreso.agen=saldo_agencia.obse  AND ingreso.fech=saldo_agencia.fech
+
+			-- EGRESO
+			LEFT JOIN (
+				SELECT agen, fech, SUM(mont) AS monto FROM movimiento_egreso
+				GROUP BY agen,fech
+			)
+			AS egreso ON
+			egreso.agen=saldo_agencia.obse  AND egreso.fech=saldo_agencia.fech
+
+			-- PRESTAMOS
+			LEFT JOIN (
+				SELECT agen, fech, SUM(mont) AS monto, SUM(mcuo) AS cuota FROM movimiento_prestamo
+				GROUP BY agen,fech)
+			AS prestamo ON
+			prestamo.agen=saldo_agencia.obse AND prestamo.fech=saldo_agencia.fech
+
+			-- VIENEN
+			LEFT JOIN cobrosypagos ON cobrosypagos.fech=saldo_agencia.fech
+			AND cobrosypagos.agen=saldo_agencia.obse
+			ORDER BY saldo_agencia.fech `
 	}
 	s = `
 		SELECT saldo_agencia.obse, saldo_agencia.saldo,
@@ -360,8 +420,8 @@ func (p *Pago) GenerarCobrosYPagosSistemas(data Pago) (jSon []byte, err error) {
 //Estado de Cuenta por Loteria y Parley (Sistemas)
 func generarCobrosYPagosSistemas(data Pago) (s string) {
 	var fecha string = ""
+	fecha = ` AND lotepar.fech BETWEEN '` + data.Desde + ` 00:00:00'::TIMESTAMP AND '` + data.Hasta + ` 23:59:59'::TIMESTAMP`
 	if data.Desde != "" {
-		fecha = ` AND lotepar.fech BETWEEN '` + data.Desde + ` 00:00:00'::TIMESTAMP AND '` + data.Hasta + ` 23:59:59'::TIMESTAMP`
 	}
 
 	s = `

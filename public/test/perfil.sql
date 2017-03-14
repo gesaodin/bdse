f67cf7fe48f14a94eabf2a4e27064349671f629a
Carlos Peña <gesaodin@gmail.com>
2017-03-14 06:33:02 -0400

    Control de Servicios

--- public/test/perfil.sql
+++ public/test/perfil.sql
@@ -48,5 +48,47 @@ JOIN (
 WHERE l.fech = (SELECT fech FROM cobrosypagoscierre ORDER BY fech desc LIMIT 1)
 GROUP BY g.obse
 
+-- Relación completa
+SELECT g.obse, g.fneg, g.trip, g.term, g.qued, g.part,
+			COALESCE(g.calc, 0) as calc,
+			COALESCE(g.freq, 0) as freq,
+			COALESCE(g.tipo, 0) as tipo,
+			COALESCE(zr.parro, 0) as parro,
+		 	zr.casa, zr.dire, zr.cuen, zr.tele, zr.celu, zr.obse, zr.fech,
+		 	COALESCE(s.venta, 0) AS venta, 
+		 	COALESCE(s.premio, 0) AS premio, 
+		 	COALESCE(s.comision, 0) AS comision, 
+		 	COALESCE(s.saldo, 0) AS saldo 
+		FROM grupo g
+		LEFT JOIN zr_gsca_localizacion zr ON g.oid=zr.grupo
+		LEFT JOIN (
+			SELECT 
+				g.oid, 
+				SUM(l.vent) AS venta,
+				SUM(l.prem) AS premio,
+				SUM(l.comi) AS comision,
+				SUM(l.saldo) AS saldo
+			FROM grupo g 
+			JOIN zr_agencia z ON g.oid=z.grupo
+			JOIN (
+				SELECT 
+					arch, agen, fech, vent-prem-comi as saldo, vent, prem, comi, sist from loteria 
+				UNION
+				SELECT 
+					arch, agen, fech, vent-prem-comi as saldo, vent, prem, comi, sist from parley
+				
+			) AS l ON z.codi=l.agen
+			WHERE l.fech = (SELECT fech FROM cobrosypagoscierre ORDER BY fech desc LIMIT 1)
+			GROUP BY g.oid
+		) AS s ON s.oid = g.oid
 
-(SELECT fech FROM cobrosypagoscierre ORDER BY fech desc LIMIT 1)
+
+
+
+
+
+
+
+
+
+

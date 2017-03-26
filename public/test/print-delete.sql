1a80d0b522cf51a568b14c9ca1f4a7115f8929bd
Carlos Peña <gesaodin@gmail.com>
2017-03-25 20:59:06 -0400

    Imprimir eliminar

---
+++ public/test/print-delete.sql
@@ -0,0 +1,17 @@
+﻿
+delete from cobrosypagos where oid>4956
+delete from cobrosypagos_grupo where oid>546
+
+delete from cobrosypagos_agencia
+insert into cobrosypagos_agencia  (oida,fech,vien,van)
+select oida,'2016-12-31'::TIMESTAMP,vien,vien from cobrosypagos WHERE fech='2016-12-31'
+
+
+insert into cobrosypagos_grupo  (oidg,fech,vien,van,movi,sald,erec)
+SELECT grupo.oid,'2016-12-31'::TIMESTAMP, SUM(vien), SUM(vien),0,0,0 FROM grupo
+JOIN agencia ON grupo.oid=agencia.grupo
+JOIN cobrosypagos ON cobrosypagos.oida=agencia.oid
+group by grupo.oid
+
+
+select * from cobrosypagos_agencia  where oida =105

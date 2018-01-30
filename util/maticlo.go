0d89f1410748e7a378e995f6c173011e0c1ffcb1
Carlos Peña <gesaodin@gmail.com>
2018-01-30 09:38:38 -0400

    Test: Ejemplo III

--- util/maticlo.go
+++ util/maticlo.go
@@ -49,7 +49,7 @@ func (a *Archivo) LeerMaticlo(ch chan []byte, tipo string) (bool, string) {
 			if a.CantidadLineas > 7 {
 				contar++
 				for _, cell := range row.Cells {
-					fmt.Println("Testing")
+					fmt.Println("Testing Operativo")
 					text := cell.String()
 					if strings.Trim(text, " ") != "" {
 						cel = append(cel, text)
